3d00331e25abed4b61dbcdde8ea1f35f5e3883bf
jserin <jsr3643@naver.com>
2024-02-02 17:00:06 +0900

    240202 calendar todolist finish

--- 3-4주차/calendar/calendar.js
+++ 3-4주차/calendar/calendar.js
@@ -41,7 +41,7 @@ const calendarModule = (function() {
 
         dates.forEach((date, i) => {
             const condition = i >= thisFirstIndex && i < thisLastIndex + 1 ? 'date' : 'other';
-            dates[i] = `<div class=${condition}><span>${date}</span></div>`;
+            dates[i] = `<div class=${condition}><span>${date}</span><span class="listDot">•</span></div>`;
         });
         
         document.querySelector('.dates').innerHTML = dates.join('');
@@ -50,12 +50,47 @@ const calendarModule = (function() {
         const today = new Date();
         if (thisMonth == today.getMonth() && thisYear == today.getFullYear()){
             for (let date of document.querySelectorAll('.date')) {
-                if (+date.innerText == today.getDate()) {
+                if (+date.childNodes[0].textContent == today.getDate()) {
                     date.classList.add('today');
                     break;
                 }
             }
         }
+
+        // 클릭한 날짜 표시
+        const dateElements = document.querySelectorAll(".date span");
+        const thisListDate = document.querySelector('.this-date');
+            
+        dateElements.forEach(dateElement => {
+            dateElement.addEventListener('click', function(event) {
+                dateElements.forEach(element => {
+                    element.style.background = '';
+                });
+
+                event.target.style.background = "white"
+
+                // 일정 날짜 출력
+                if (document.querySelector('.viewTdDate')) {
+                    thisListDate.lastElementChild.remove();
+                }
+
+                let viewTdDate = document.createElement('span');
+                viewTdDate.classList.add('viewTdDate');
+                viewTdDate.innerHTML = `${date.getFullYear()}.${date.getMonth()+1}.${event.target.textContent}`
+                thisListDate.append(viewTdDate);
+
+                // 일정 리스트 출력
+                const todoListUl = document.querySelectorAll('#todoListUl li');
+                todoListUl.forEach(list => {
+                    list.classList.remove('hidden');
+                    if (list.className != viewTdDate.textContent) {
+                        list.classList.add('hidden');
+                    }
+                })
+            });
+        })
+
+        ///////////////// 20240202 일정 있는 곳 표시 ------ 해야함
     }
 
     function init() {
@@ -79,6 +114,35 @@ const calendarModule = (function() {
             `;
             calendarElement.append(calendarBox);
     
+            // 검색&일정 부분
+            const eventElement = document.querySelector('body');
+            const thisYear = date.getFullYear();
+            const thisMonth = date.getMonth();
+            const thisDate = date.getDate();
+
+            let searchBox = document.createElement('div');
+            searchBox.classList.add('event-area');
+            searchBox.innerHTML = `
+                <div class="search-area">
+                    <input type="number" value="${thisYear}" id="inputYear" default="${thisYear}" min="1" max="3000">
+                    <input type="number" value="${thisMonth+1}" id="inpurMonth" default="${thisMonth+1}" min="1" max="12">
+                    <button class="search-btn">검색</button>
+                </div>
+                <div class="todoList-area">
+                    <div class="this-date"><span>TODO</span><span class="viewTdDate">${thisYear}.${thisMonth+1}.${thisDate}</span></div>
+                    <div class="todolist">
+                        <div class="add-area">
+                            <input type="text" placeholder="TODOLIST 추가하기" id="inputSchedule" maxlength="100">
+                            <button id="addList-btn">추가</button>
+                        </div>
+                        <div class="view-list">
+                            <ul id="todoListUl"></ul>
+                        </div>
+                    </div>
+                </div>
+            `;
+            eventElement.append(searchBox);
+            
             // 이전달 이동
             document.getElementById("prev-month").addEventListener("click", () => {
                 date.setMonth(date.getMonth() - 1);
@@ -98,36 +162,40 @@ const calendarModule = (function() {
             });
 
 
-            // 검색&일정 부분
-            const eventElement = document.querySelector('body');
-            const thisYear = date.getFullYear();
-            const thisMonth = date.getMonth();
-            let searchBox = document.createElement('div');
-            searchBox.classList.add('event-area');
-            searchBox.innerHTML = `
-                <div class="search-area">
-                    <input type="number" placeholder="${thisYear}">
-                    <input type="number" placeholder="${thisMonth+1}">
-                    <button class="search-btn">검색</button>
-                </div>
-                <div class="todoList-area"></div>
-            `;
-            eventElement.append(searchBox);
+            // 년, 월로 검색
+            const searchBtn = document.querySelector(".search-btn");
+            const inputYear = document.getElementById("inputYear");
+            const inputMonth = document.getElementById("inpurMonth");
+
+            searchBtn.addEventListener("click", () => {
+                if (inputYear.value == 0 || inputYear.value == null || inputYear.value < 1 || inputYear.value > 10000){
+                    date.setMonth(inputMonth.value - 1);
+                } else if(inputMonth.value == 0 || inputMonth.value == null || inputMonth.value < 1 || inputMonth > 12) {
+                    date.setFullYear(inputYear.value);
+                } else {
+                    date.setFullYear(inputYear.value);
+                    date.setMonth(inputMonth.value - 1);
+                }
+                render();
+            })
 
 
+            // 일정 추가하기
+            const addBtn = document.getElementById("addList-btn");
+            const inputSchedule = document.getElementById("inputSchedule");
+            const scheduleBox = document.querySelector('.view-list ul');
+           
 
-            ////20230201 여기까지 함 날짜 선택하면 색 바뀌고 인풋에 놓기
-            const datesContainer = document.querySelector(".dates");
-            datesContainer.addEventListener('click', function(dateElement) {
-                if (dateElement.target.classList.contains('date')) {
-                    const allDates = document.querySelectorAll('.date');
-                    allDates.forEach(date => {
-                        date.style.backgroundColor = '';
-                    });
-
-                    event.target.style.backgroundColor = 'lightblue';
+            addBtn.addEventListener("click", () => {
+                let selectedDate = document.querySelector('.viewTdDate');
+                if (inputSchedule.value) {
+                    let todoContent = document.createElement('li');
+                    todoContent.classList.add(selectedDate.textContent);
+                    todoContent.innerHTML = inputSchedule.value;
+                    scheduleBox.append(todoContent);
                 }
 
+                inputSchedule.value = ""
             })
 
             render();
